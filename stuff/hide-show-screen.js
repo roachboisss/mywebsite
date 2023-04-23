284f8d6fc865014cfed76438fc4bce5cf0eace3b
Phillip <108208254+roachboisss@users.noreply.github.com>
2023-04-22 21:01:44 -0700

    Add files via upload

---
+++ stuff/hide-show-screen.js
@@ -0,0 +1,10 @@
+function dim() {
+    document.getElementById("dimmer").style.display = "block";
+    document.getElementById("showscreen").style.display = "block";
+    document.getElementById("hidescreen").style.display = "none";
+  }
+  function undim() {
+    document.getElementById("dimmer").style.display = "none";
+    document.getElementById("showscreen").style.display = "none";
+    document.getElementById("hidescreen").style.display = "block";
+  }
